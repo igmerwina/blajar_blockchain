2fb515c979c19bbeba961fe85a12095e1c143897
igmerwina <igmerwina@gmail.com>
2018-09-19 21:19:55 +0700

    formatted python3 syntax with yapf

---
+++ README.md
@@ -0,0 +1,9 @@
+# Blajar Blockchain 
+
+![blockchain](https://cdn-images-1.medium.com/max/800/1*3hyWN8UhcrL7P0Opbu7IQg.jpeg)
+
+Repo ini saya gunakan untuk belajar memahami teknologi blockchain. Sumber video dari [Udemy: Academind by Maximilian Schwarzmüller](https://www.udemy.com/learn-python-by-building-a-blockchain-cryptocurrency/)
+
+Akan terus diupdate
+
+# Semangat Belajar!!! :fire:
--- blockchain.py
+++ blockchain.py
@@ -1,25 +1,30 @@
 blockchain = []
 
+
 def get_last_blockchain_value():
-    return blockchain[-1]
+    return blockchain[-1] # index list paling awal 
 
 
 def add_value(transaction_ammount, last_transaction=[1]):
+    # nambahin last_trx sama tx_ammount ke bc
     blockchain.append([last_transaction, transaction_ammount])
 
 
 def get_user_input():
+    # input buat user masukin data ke bc
     return float(input('Your transaction ammount please: '))
 
 
-tx_ammount=get_user_input()
-add_value(tx_ammount) # transaksi pertama  
+tx_ammount = get_user_input()
+add_value(tx_ammount)  # transaksi pertama
 
-tx_ammount=get_user_input()
+tx_ammount = get_user_input()
 # transaksi sebelumnya , transaksi sekarang
-add_value(last_transaction=get_last_blockchain_value(), transaction_ammount=tx_ammount) 
+add_value(
+    last_transaction=get_last_blockchain_value(),
+    transaction_ammount=tx_ammount)
 
-tx_ammount=get_user_input()
-add_value(tx_ammount, get_last_blockchain_value()) 
+tx_ammount = get_user_input()
+add_value(tx_ammount, get_last_blockchain_value())
 
 print(blockchain)
