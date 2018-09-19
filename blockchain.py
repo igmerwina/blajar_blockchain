9a8115b6320ac0b4357b69c3fbade00ac930d21b
igmerwina <igmerwina@gmail.com>
2018-09-19 13:31:56 +0700

    add last_transaction to function call

--- blockchain.py
+++ blockchain.py
@@ -1,15 +1,15 @@
-blockchain = [[1]]
+blockchain = []
 
 def get_last_blockchain_value():
     return blockchain[-1]
 
 
-def add_value(transaction_ammount):
-    blockchain.append([get_last_blockchain_value(), transaction_ammount])
+def add_value(transaction_ammount, last_transaction=[1]):
+    blockchain.append([last_transaction, transaction_ammount])
 
 
 add_value(2) 
-add_value(0.9) 
-add_value(10.89) 
+add_value(last_transaction=get_last_blockchain_value(), transaction_ammount=0.9) 
+add_value(10.89, get_last_blockchain_value()) 
 
 print(blockchain)
