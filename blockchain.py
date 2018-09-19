a94a30a6098b21b8b3c15411ecbec11cb467158e
igmerwina <igmerwina@gmail.com>
2018-09-19 13:40:22 +0700

    input: tx_ammount

--- blockchain.py
+++ blockchain.py
@@ -8,8 +8,14 @@ def add_value(transaction_ammount, last_transaction=[1]):
     blockchain.append([last_transaction, transaction_ammount])
 
 
-add_value(2) 
-add_value(last_transaction=get_last_blockchain_value(), transaction_ammount=0.9) 
-add_value(10.89, get_last_blockchain_value()) 
+tx_ammount=float(input('Your transaction ammount plesase: '))
+add_value(tx_ammount) # transaksi pertama  
+
+tx_ammount=float(input('Your transaction ammount plesase: '))
+# transaksi sebelumnya , transaksi sekarang
+add_value(last_transaction=get_last_blockchain_value(), transaction_ammount=tx_ammount) 
+
+tx_ammount=float(input('Your transaction ammount plesase: '))
+add_value(tx_ammount, get_last_blockchain_value()) 
 
 print(blockchain)
