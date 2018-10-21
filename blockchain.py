bbae9a304add69b82c6bb07717c8ddd8e63500b9
igmerwina <igmerwina@gmail.com>
2018-10-21 13:20:10 +0700

    add docstring on blockchian.py

--- blockchain.py
+++ blockchain.py
@@ -1,16 +1,25 @@
+# initiate blockchain list
 blockchain = []
 
 
 def get_last_blockchain_value():
-    return blockchain[-1] # index list paling awal 
+    """ Return the last value of the current blockchain. """
+    return blockchain[-1]  # index list paling awal
 
 
 def add_value(transaction_ammount, last_transaction=[1]):
+    """ Append a new value as well as the last blockchain value to the blockchain 
+    
+    Argument:
+        :transaction_ammount: The ammount that should be added.
+        :last_transaction: The last blockchain transaction (default[1]).
+    """
     # nambahin last_trx sama tx_ammount ke bc
     blockchain.append([last_transaction, transaction_ammount])
 
 
 def get_user_input():
+    """ Returns the input of the user (a new transaction amount) as a float. """
     # input buat user masukin data ke bc
     return float(input('Your transaction ammount please: '))
 
