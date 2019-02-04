7923aaef24db6a1af56e9e2bc48ea4c047e39904
igmerwina <igmerwina@gmail.com>
2019-02-04 14:22:45 +0700

    Add simpler logic to verify block

--- blockchain.py
+++ blockchain.py
@@ -9,7 +9,8 @@ def get_last_blockchain_value():
     return blockchain[-1]  # index list paling awal
 
 
-def add_transaction(transaction_ammount, last_transaction=[1]): # [1] = default value
+def add_transaction(transaction_ammount,
+                    last_transaction=[1]):  # [1] = default value
     """ Append a new value as well as the last blockchain value to the blockchain 
     
     Argument:
@@ -19,10 +20,9 @@ def add_transaction(transaction_ammount, last_transaction=[1]): # [1] = default 
     # nambahin last_trx sama tx_ammount ke bc
     # blockchainnya di appen oleh last trasaction sama tx_ammount
     # last trasaction karena baru, jadi di isi [1] biar keisi dulu
-    if last_transaction == None: 
+    if last_transaction == None:
         last_transaction = [1]
     blockchain.append([last_transaction, transaction_ammount])
-    
 
 
 def get_transaction_value():
@@ -43,47 +43,62 @@ def print_blockchain_elements():
     for block in blockchain:
         print('Outputing Block')
         print(block)
+    else:  # else dieksekusi setelah for selesai
+        print('-' * 25)
 
 
 def verify_chain():
     # Blockchain looping logic for verification
-    block_index = 0 # inisiasi index block 
-    is_valid = True # helper variable buat ngecek apa bener last block ada di first block
-    for block in blockchain:
-        if block_index == 0: 
-            block_index += 1 # kalau block_index = 0, data +1
+    is_valid = True  # helper variable buat ngecek apa bener last block ada di first block
+    for block_index in range(len(blockchain)):
+        if block_index == 0:
             continue
-        if block[0] == blockchain[block_index - 1]:
-            is_valid = True # ngecek last block apa ada di first block
-        else: 
+        elif blockchain[block_index][0] == blockchain[block_index - 1]:
+            is_valid = True  # ngecek last block apa ada di first block
+        else:
             is_valid = False
             break
-        block_index += 1
-    return is_valid 
+
+    # --- Logic versi lama ---
+    # for block in blockchain:
+    #     if block_index == 0:
+    #         block_index += 1 # kalau block_index = 0, data +1
+    #         continue
+    #     if block[0] == blockchain[block_index - 1]:
+    #         is_valid = True # ngecek last block apa ada di first block
+    #     else:
+    #         is_valid = False
+    #         break
+    #     block_index += 1
+
+    return is_valid
 
 
-# Loop the trasaction   
-while True:
+waiting_for_input = True
+
+# Loop the trasaction
+while waiting_for_input:
     print('\nPlease Choose')
     print('1. Add a new transaction value')
     print('2. Output the Blockchain Blocks')
     print('h. Manipulate the chain')
     print('q. Quit')
     user_choice = get_user_choice()
-    if user_choice == '1': 
+    if user_choice == '1':
         tx_ammount = get_transaction_value()
         add_transaction(tx_ammount, get_last_blockchain_value())
-    elif user_choice == '2': 
+    elif user_choice == '2':
         print_blockchain_elements()
-    elif user_choice == 'h': 
+    elif user_choice == 'h':
         # manipulate block
-        if len(blockchain) >1: 
+        if len(blockchain) > 1:
             blockchain[0] = [2]
     elif user_choice == 'q':
-        break # 'break' quit the loop  
-    else: 
+        waiting_for_input = False
+    else:
         print('Input was invalid, please pick a value from the list!')
     if not verify_chain():
+        print_blockchain_elements()
         print('Invalid Blockhain')
         break
 
