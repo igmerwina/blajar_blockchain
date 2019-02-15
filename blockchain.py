229493ad5bdc2872a8eac68c20272b5471fc18e2
igmerwina <igmerwina@gmail.com>
2019-02-15 14:40:51 +0700

    Add file access to the Blockchain

--- blockchain.py
+++ blockchain.py
@@ -26,6 +26,25 @@ owner = 'Max'
 participants = {'Max'}  
 
 
+def load_data():
+    with open('blockchain.txt', mode='r') as f:
+        file_content = f.readlines()
+        global blockchain
+        global open_transactions
+        blockchain = file_content[0]
+        open_transactions = file_content[1]
+
+
+load_data()
+
+
+def save_data():
+    with open('blockchain.txt', mode='w') as f:
+        f.write(str(blockchain))
+        f.write('\n')
+        f.write(str(open_transactions))
+
+
 def valid_proof(transactions, lats_hash, proof):
     guess = (str(transactions) + str(lats_hash) + str(proof)).encode()
     guess_hash = hash_string_256(guess)
@@ -95,6 +114,7 @@ def add_transaction(recipient, sender=owner, amount=1.0):
         open_transactions.append(transaction)  # data di transaction dimasukin ke open_transactions
         participants.add(sender)  # add
         participants.add(recipient)
+        save_data()
         return True
     return False
 
@@ -123,6 +143,7 @@ def mine_block():
         'proof': proof
     }
     blockchain.append(block)
+    save_data()
     return True  # reset open_transactions to empty list
 
 
